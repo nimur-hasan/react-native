67a81af7f5114abd586c9923db403e7ae5cfef2f
nimur-hasan <developernaim20@gmail.com>
2023-09-15 00:33:47 +0600

    Alert - Dialog Popup

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import { useState } from "react";
-import { ActivityIndicator, Button, Image, ImageBackground, Modal, Pressable, ScrollView, StatusBar, Text, Touchable, View } from "react-native";
+import { ActivityIndicator, Alert, Button, Image, ImageBackground, Modal, Pressable, ScrollView, StatusBar, Text, Touchable, View } from "react-native";
 const logoImage = require('./assets/icon.png');
 
 export default function App (){
 
-  const [openModal, setOpenModal] = useState(false)
 
   return <View style={{flex: 1, backgroundColor: 'plum', padding:60}}>
-    <Button title="Press Here" onPress={() => setOpenModal(true)} color={'midnightblue'}/>
-    
-    <ActivityIndicator 
-      size='large' 
-      color={'midnightblue'} 
-      animating={true} // predefined animating true, controlled by state.
-    /> 
+    <Button title="Press Here" onPress={() => Alert.alert("Update Error", "Failed to update", [
+      {
+        text: "Cancel",
+        onPress: () => {console.log('Cancell Pressed')}
+      },
+      {
+        text: "Accept",
+        onPress: () => {console.log('Accept Pressed')}
+      }
+    ])} color={'midnightblue'}/>
 
     
   </View>

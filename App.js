e21719d892be82f28d69931b9a33c4f4e81892be
nimur-hasan <developernaim20@gmail.com>
2023-09-14 20:41:08 +0600

    ActivityIndicator - Loading Animation

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Image, ImageBackground, Modal, Pressable, ScrollView, StatusBar, Text, Touchable, View } from "react-native";
+import { ActivityIndicator, Button, Image, ImageBackground, Modal, Pressable, ScrollView, StatusBar, Text, Touchable, View } from "react-native";
 const logoImage = require('./assets/icon.png');
 
 export default function App (){
@@ -9,9 +9,10 @@ export default function App (){
   return <View style={{flex: 1, backgroundColor: 'plum', padding:60}}>
     <Button title="Press Here" onPress={() => setOpenModal(true)} color={'midnightblue'}/>
     
-    <StatusBar 
-      backgroundColor={'lightblue'} //backgroundColor only affects on android
-      barStyle={'dark-content'} //barStyle ["light-content", "dark-content"]
+    <ActivityIndicator 
+      size='large' 
+      color={'midnightblue'} 
+      animating={true} // predefined animating true, controlled by state.
     /> 
 
     

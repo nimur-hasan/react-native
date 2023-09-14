b97d0fb59c3abfb6d82a5a1de01233b98e16f8f2
nimur-hasan <developernaim20@gmail.com>
2023-09-14 20:26:57 +0600

    StatusBar

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Image, ImageBackground, Modal, Pressable, ScrollView, Text, Touchable, View } from "react-native";
+import { Button, Image, ImageBackground, Modal, Pressable, ScrollView, StatusBar, Text, Touchable, View } from "react-native";
 const logoImage = require('./assets/icon.png');
 
 export default function App (){
@@ -7,19 +7,13 @@ export default function App (){
   const [openModal, setOpenModal] = useState(false)
 
   return <View style={{flex: 1, backgroundColor: 'plum', padding:60}}>
-    <Button title="Open Modal" onPress={() => setOpenModal(true)} color={'midnightblue'}/>
+    <Button title="Press Here" onPress={() => setOpenModal(true)} color={'midnightblue'}/>
     
-    <Modal 
-      visible={openModal} 
-      onRequestClose={() => setOpenModal(false)}
-      animationType="slide" // animation types [slide, fade]
-      presentationStyle="pageSheet" // presentation styles [pageSheet, formSheet] #only for ios
-    >
-      <View style={{backgroundColor: 'lightblue', padding: 60, flex: 1}}>
-        <Text style={{fontSize: 40}}>Modal Content</Text>
-        <Button onPress={() => setOpenModal(false)} title="Close"/>
-      </View>
-    </Modal>
+    <StatusBar 
+      backgroundColor={'lightblue'} //backgroundColor only affects on android
+      barStyle={'dark-content'} //barStyle ["light-content", "dark-content"]
+    /> 
+
     
   </View>
-} 
+}

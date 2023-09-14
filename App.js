39868ae4cbe3fcc58422c7517de40daa8bca7f16
nimur-hasan <developernaim20@gmail.com>
2023-09-14 20:14:28 +0600

    Modal - Slide Up amazing features

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
-import { Button, Image, ImageBackground, Pressable, ScrollView, Text, Touchable, View } from "react-native";
+import { useState } from "react";
+import { Button, Image, ImageBackground, Modal, Pressable, ScrollView, Text, Touchable, View } from "react-native";
 const logoImage = require('./assets/icon.png');
 
 export default function App (){
+
+  const [openModal, setOpenModal] = useState(false)
+
   return <View style={{flex: 1, backgroundColor: 'plum', padding:60}}>
-    <Button title="Click Here" onPress={() => console.log('Pressed')} color={'midnightblue'}/>
+    <Button title="Open Modal" onPress={() => setOpenModal(true)} color={'midnightblue'}/>
     
-      <Pressable onPressIn={() => {console.log('Pressed on Image In')}} onPressOut={() => {console.log('Pressed on Image Out')}} onLongPress={() => {console.log('Long Pressed on Image In')}}>
-        <Image source={logoImage} style={{height: 300, width: '100%'}}/>
-      </Pressable>
+    <Modal 
+      visible={openModal} 
+      onRequestClose={() => setOpenModal(false)}
+      animationType="slide" // animation types [slide, fade]
+      presentationStyle="pageSheet" // presentation styles [pageSheet, formSheet] #only for ios
+    >
+      <View style={{backgroundColor: 'lightblue', padding: 60, flex: 1}}>
+        <Text style={{fontSize: 40}}>Modal Content</Text>
+        <Button onPress={() => setOpenModal(false)} title="Close"/>
+      </View>
+    </Modal>
     
   </View>
 } 
